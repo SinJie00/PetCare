809ce642e421fa10bd3ebe206ea092ad113ed705
SinJIe00 <limsinjie@graduate.utm.my>
2023-05-03 00:53:01 +0800

    delete old code

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,19 +1,6 @@
-/* import Vue from 'vue'
-import Vuex from 'vuex' */
 import createPersistedState from 'vuex-persistedstate'
 import { createStore } from 'vuex'
 import auth from './auth'
-
-/* Vue.use(Vuex) */
-
-/* export default new Vuex.Store({
-    plugins:[
-        createPersistedState()
-    ],
-    modules:{
-        auth
-    }
-}) */
 
 const store = createStore({
     plugins: [
