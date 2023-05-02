9bb8c7e3ad0f605bbf9bb780932ed21c17e5e36d
SinJIe00 <limsinjie@graduate.utm.my>
2023-05-03 00:52:38 +0800

    update adoption animal naming

--- resources/js/pages/index.js
+++ resources/js/pages/index.js
@@ -2,9 +2,9 @@ import Home from './public/Home.vue';
 import Login from './public/Login.vue';
 import Register from './public/Register.vue';
 import Profile from './public/Profile.vue';
+import AdoptionAnimal from './admin/AdoptionAnimal.vue';
 import AdoptionApplication from './admin/AdoptionApplication.vue';
-import AdoptionList from './admin/AdoptionList.vue';
-import DonationAdmin from './admin/DonationAdmin.vue';
+import ProductDonationAdmin from './admin/ProductDonationAdmin.vue';
 import StrayPostAdmin from './admin/StrayPostAdmin.vue';
 import VolunteerApplicationAdmin from './admin/VolunteerApplicationAdmin.vue';
 import ArticleAdmin from './admin/ArticleAdmin.vue';
@@ -20,9 +20,9 @@ export {
     Login,
     Register,
     Profile,
+    AdoptionAnimal,
     AdoptionApplication,
-    AdoptionList,
-    DonationAdmin,
+    ProductDonationAdmin,
     StrayPostAdmin,
     ArticleAdmin,
     VolunteerApplicationAdmin,
