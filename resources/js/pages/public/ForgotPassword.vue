8f425082f7e13c1679e4c66b3591c0c70b3f2764
SinJIe00 <limsinjie@graduate.utm.my>
2023-06-30 00:06:49 +0800

    update validation

--- resources/js/pages/public/ForgotPassword.vue
+++ resources/js/pages/public/ForgotPassword.vue
@@ -5,10 +5,15 @@
         <div class="card mt-5">
           <div class="card-body">
             <h2 class="text-center mb-4">Forgot Password</h2>
-            <form @submit.prevent="submitForm">
+            <form @submit.prevent="submitForm" novalidate>
               <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" v-model="email" required>
+                <input type="email" class="form-control" id="email" v-model="email" required
+                  :class="{ 'is-invalid': v$.email.$error }" @blur="v$.email.$touch">
+                <div v-for="(error, index) of v$.email.$errors" :key="index" class="invalid-feedback">
+                  <div v-if="error.$validator == 'required'">Email is required.</div>
+                  <div v-else-if="error.$validator == 'email'">A valid email is required.</div>
+                </div>
               </div>
               <div class="text-center">
                 <button type="submit" class="btn btn-primary">Reset Password</button>
@@ -22,7 +27,18 @@
 </template>
   
 <script>
+import { useVuelidate } from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators';
 export default {
+  setup() {
+    const v$ = useVuelidate();
+    return { v$ };
+  },
+  validations() {
+    return {
+      email: { required, email },
+    }
+  },
   data() {
     return {
       email: "",
@@ -31,16 +47,30 @@ export default {
   methods: {
     submitForm() {
       // Make a POST request to your Laravel API endpoint to initiate the password reset process
-      axios.post("/api/forgot-password", {
-        email: this.email,
-      })
-        .then(() => {
-          alert("Password reset link sent to your email.");
+      console.log('submit form');
+      console.log(this.v$.email.$error);
+      if (!this.v$.email.$error) {
+        console.log('hi');
+        axios.post("/api/forgot-password", {
+          email: this.email,
         })
-        .catch((error) => {
-          console.error(error);
-          alert("Failed to send password reset link.");
-        });
+          .then(() => {
+            this.$swal({
+              title: 'Confirmation',
+              text: 'Password reset link sent to your email.',
+              icon: 'success',
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+            this.$swal({
+              title: 'Alert Message',
+              text: 'Failed to send password reset link. Please try again.',
+              icon: 'error',
+            });
+            //alert("Failed to send password reset link.");
+          });
+      }
     },
   },
 };
--- resources/js/pages/public/ResetPassword.vue
+++ resources/js/pages/public/ResetPassword.vue
@@ -7,17 +7,33 @@
                         <h2 class="text-center mb-5">Reset Password</h2>
                         <form @submit.prevent="resetPassword" novalidate>
                             <div class="form-group mt-4">
-                                <label for="password">Password</label>
-                                <input id="password" required v-model="password" type="password" class="form-control">
+                                <label for="password" class="form-label">Password</label>
+                                <input id="password" required v-model="password" type="password" class="form-control"
+                                    :class="{ 'is-invalid': errors && errors.password }">
+                                    <!-- @blur="v$.password.$touch">
+                                <div v-for="(error, index) of v$.password.$errors" :key="index" class="invalid-feedback">
+                                    <div v-if="error.$validator == 'required'">Password is required.</div>
+                                    <div v-else-if="error.$validator == 'minLength'">The password must be at least 8 characters.</div>
+                                    <div v-else-if="error.$validator == 'regex'">The password must contain at least one uppercase letter, one number, and one special character.</div>
+                                </div> -->
                                 <!-- <span class="text-danger" v-if="passwordError">{{ passwordError }}</span> -->
+                                <div v-if="errors && errors.password" class="invalid-feedback">{{ errors.password[0] }}</div>
                             </div>
                             <div class="form-group mt-4">
                                 <label for="confirm_password">Confirm Password</label>
-                                <input id="confirm_password" required v-model="confirm_password" type="password" class="form-control">
+                                <input id="confirm_password" required v-model="confirm_password" type="password"
+                                    class="form-control" :class="{ 'is-invalid': errors && errors.confirm_password }">
+                                    <!-- @blur="v$.confirm_password.$touch">
+                                <div v-for="(error, index) of v$.confirm_password.$errors" :key="index" class="invalid-feedback">
+                                    <div v-if="error.$validator == 'required'">Confirmation Password is required.</div>
+                                    <div v-else-if="error.$validator == 'sameas'">The confirmation password does not match.</div>
+                                </div> -->
+                                <div v-if="errors && errors.confirm_password" class="invalid-feedback">{{ errors.confirm_password[0] }}</div>
                                 <!-- <span class="text-danger" v-if="confirmPasswordError">{{ confirmPasswordError }}</span> -->
                             </div>
                             <div class="text-center mt-4">
-                                <button type="submit" class="btn btn-lg btn-primary btn-block w-75 mx-auto">Reset Password</button>
+                                <button type="submit" class="btn btn-lg btn-primary btn-block w-75 mx-auto">Reset
+                                    Password</button>
                             </div>
                         </form>
                     </div>
@@ -28,11 +44,31 @@
 </template>
   
 <script>
+/* import { useVuelidate } from '@vuelidate/core';
+import { required, minLength, regex, sameAs } from '@vuelidate/validators'; */
 export default {
+    /* setup() {
+        const v$ = useVuelidate();
+        return { v$ };
+    },
+    validations() {
+        return {
+            password: {
+                required,
+                minLength: minLength(8),
+                regex: regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)
+            },
+            confirm_password: {
+                required,
+                sameAsPassword: sameAs('password')
+            }
+        }
+    }, */
     data() {
         return {
             password: '',
             confirm_password: '',
+            errors: {}
         };
     },
     /* mounted() {
@@ -47,22 +83,36 @@ export default {
                 alert('Password and confirm password do not match.');
                 return;
             } */
-
+            this.errors = {};
             // Send an API request to update the user's password
             axios.post('/api/reset-password', {
                 token: this.$route.query.token,
                 email: this.$route.query.email,
                 password: this.password,
                 confirm_password: this.confirm_password,
-            })
-                .then(() => {
-                    alert('Password has been reset successfully.');
+            }).then(() => {
+                    //alert('Password has been reset successfully.');
+                    this.$swal({
+                        title: 'Confirmation',
+                        text: 'Password has been reset successfully.',
+                        icon: 'success',
+                    });
                     // Redirect the user to the login page or any other appropriate page
                     this.$router.push({ name: 'Login' }); // Redirect to Login page
                 })
                 .catch(error => {
-                    console.error(error);
-                    alert('Failed to reset password. Please try again.');
+                    if (error.response && error.response.status === 422) {
+                        this.errors  = error.response.data.errors;
+                        console.log(this.errors);
+                    } 
+                    else{
+                    this.$swal({
+                        title: 'Alert Message',
+                        text: 'Failed to reset password. Please try again later.',
+                        icon: 'error',
+                    });
+                    }
+                    //alert('Failed to reset password. Please try again.');
                 });
         },
     },
